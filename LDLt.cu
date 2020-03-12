d67f5484299997421283a00c00d44bf0c82d4aaa
PierreGtch <pierre.guetschel@gmail.com>
2020-03-13 00:50:07 +0100

    LDLt fonctionne (version colonnes

--- LDLt.cu
+++ LDLt.cu
@@ -1,14 +1,89 @@
 # include "utils.h"
 
 
+
+int hgetL(int d, int i, int j) {
+    // If j > i, then we take the transpose of L
+    if (j > i) {
+        int t = i;
+        i = j;
+        j = t;
+    }
+
+    int l_position    =     i*(i+1) / 2 + j;
+    // int l_position =     i*(i-1) / 2 + j-1;
+
+    return l_position;
+}
+
+int hgetD(int i) {
+    int d_position = i;
+    return d_position;
+}
+
+void generate_systems(float *A, float *Y, int N, int d, bool verbose=true) {
+    int matrix_size = d + d * (d + 1) / 2;
+
+    for (int i = 0; i < N; i++) {
+        float *D = &A[i * matrix_size];
+        float *T = &A[i * matrix_size + d];
+
+
+        for (int j=0; j < (d * (d + 1) / 2); j++)
+            T[j] = ((float) (1+rand()%2));
+
+
+        for (int j=0; j<d; j++)
+            T[hgetL(d, j,j)] = 1.0f;
+
+        for (int j=0; j<d; j++) {
+            D[j] = (float) (1+rand()%5);
+            Y[matrix_size * i + j] = ((float) (1+rand()%2));
+        }
+
+        if (verbose)
+        {
+          printf("[");
+          for(int k = 1; k<=d; k++)
+          {
+              printf("[");
+              for(int j = 1; j<=d; j++)
+              {
+                  if (j <= k)
+                      printf("%f,", T[hgetL(d, j, k)]);
+                  else
+                      printf("%f,", 0.0f);
+              }
+              printf("],");
+          }
+          printf("]\n");
+
+          printf("[");
+          for(int k = 0; k<d; k++)
+              printf("%f,", D[k]);
+          printf("]\n");
+
+          printf("[");
+          for(int k = 0; k<d; k++)
+              printf("%f,", Y[k]);
+          printf("]\n");
+
+        }
+    }
+}
+
+// ************************************************************************ //
+
 // __device__ int getL(float* T, int n, int d, int matrix_id, int i, int j)
 __device__ int getL(int d, int i, int j)
 {
     // If j > i, then we take the transpose of L
-    if (j > i); {int t = i; i = j; j = t;};
+    if (j > i) {int t = i; i = j; j = t;};
 
     // int matrix_memory_size = (d+d*(d+1)/2);
-    int l_position = d + i*(i-1) / 2 + j;
+    int l_position    = d + i*(i+1) / 2 + j;
+    // int l_position =     i*(i-1) / 2 + j-1;
+    // int l_position = d + i*(i-1) / 2 + j;
     return l_position;
     // return &T[matrix_id * matrix_memory_size + l_position]
 }
@@ -33,19 +108,21 @@ __device__ void parallel_copy(float* dest, float* src, int n)
   __syncthreads();
 }
 
-__global__ void LDLt_max_col_k_shared(float* ACPU, int d)
+// __global__ void LDLt_max_col_k(float* AGPU, int d)
+__global__ void LDLt_max_col_k(float* sA, int d)
 {
   int tidx = threadIdx.x%d;
   int Qt = (threadIdx.x-tidx)/d;
-  int A_size = (d*d+d)/2+d;
+  int A_size = d*(d+1)/2+d;
   int minTB = blockDim.x/d;
-  int nt = Qt * A_size;
+  // printf("minTB %d\n", minTB);
+  int nt = (blockIdx.x*minTB + Qt) * A_size;
   // int gbx = Qt + blockIdx.x*(blockDim.x/d);
 
 
-  extern __shared__ float sA[];
-  //copy ACPU to sA
-  parallel_copy(sA, &ACPU[(blockIdx.x*minTB + Qt)*A_size], minTB*A_size);
+  // extern __shared__ float sA[];
+  // //copy ACPU to sA
+  // parallel_copy(sA, &AGPU[(blockIdx.x*minTB + Qt)*A_size], minTB*A_size);
 
 
 
@@ -64,6 +141,7 @@ __global__ void LDLt_max_col_k_shared(float* ACPU, int d)
 
     // L_:,j parallel
     if(tidx>j){
+      printf("(%d,%d,%d,%d),", nt+getL(d,tidx,j), nt, tidx, j);
       sA[nt+getL(d,tidx,j)] /= sA[nt+getD(d,j)];
       for(k=0; k<j; k++){
         sA[nt+getL(d,tidx,j)] -= sA[nt+getL(d,tidx,k)]*
@@ -75,31 +153,131 @@ __global__ void LDLt_max_col_k_shared(float* ACPU, int d)
     __syncthreads();
   }
 
-  parallel_copy(&ACPU[(blockIdx.x*minTB + Qt)*A_size], sA, minTB*A_size);
+  // parallel_copy(&AGPU[(blockIdx.x*minTB + Qt)*A_size], sA, minTB*A_size);
 
 }
 
 
-int main(int *argc, char*[] argv)
-{
-  int n = 5;
-  int d = 6;
-  // int num_thread_per_block = 1024;
-  int num_thread_per_block = 15; // just to test
-  int minTB = num_thread_per_block/d;
-  int NB = n/minTB;
-
-  float* A;
-  int A_size = (d*d+d)/2+d;
 
 
+// ************************************************************************ //
 
-  cudaMalloc(&A, sizeof(float)*n*A_size);
-  init_A(A,n,d);
-  print("A[0] before : %f", A[0])
 
-  <<< NB, d*minTB, minTB*A_size*sizeof(float)>>>(A, d);
+int main() {
+    float Tim;                            // GPU timer instructions
+    cudaEvent_t start, stop;            // GPU timer instructions
+    int d = 20;
+    int n = 1;
+    // int num_thread_per_block = 1024;
+    int num_thread_per_block = 300; // just to test
+    int minTB = 1;  // number of matrix per block
+    // int minTB = num_thread_per_block/d;  // number of matrix per block
+    int NB = n/minTB;  // number of blocks
 
-  print("A[0] after : %f", A[0])
+    srand(time(0));
 
+    auto *A     = (float *) malloc(sizeof(float) * n * (d + d * (d + 1) / 2));
+    auto *LandD = (float *) malloc(sizeof(float) * n * (d + d * (d + 1) / 2));
+    auto *Y = (float *) malloc(sizeof(float) * n * d);
+    // auto *Ychap = (float *) malloc(sizeof(float) * n * d);
+    auto *X = (float *) malloc(sizeof(float) * n * d);
+
+    float *gpuA;
+    float *gpuY;
+
+    cudaMalloc(&gpuA, sizeof(float) * n * (d + d * (d + 1) / 2));
+    cudaMalloc(&gpuY, sizeof(float) * n * d);
+
+    generate_systems(A, Y, n, d, false);
+
+    cudaMemcpy(gpuA, A, sizeof(float) * n * (d + d * (d + 1) / 2), cudaMemcpyHostToDevice);
+    // cudaMemcpy(gpuY, Y, sizeof(float) * n * d, cudaMemcpyHostToDevice);
+
+
+
+    cudaEventCreate(&start);                // GPU timer instructions
+    cudaEventCreate(&stop);                 // GPU timer instructions
+    cudaEventRecord(start, 0);              // GPU timer instructions
+
+    LDLt_max_col_k <<< NB, d * minTB, minTB * ((d * d + d) / 2 + d) * sizeof(float) >>> (gpuA, d);
+    cudaDeviceSynchronize();
+
+    cudaEventRecord(stop, 0);               // GPU timer instructions
+    cudaEventSynchronize(stop);             // GPU timer instructions
+    cudaEventElapsedTime(&Tim, start, stop);// GPU timer instructions
+    cudaEventDestroy(start);                // GPU timer instructions
+    cudaEventDestroy(stop);                 // GPU timer instructions
+    printf("\nExecution time %f ms\n", Tim);  // GPU timer instructions
+
+
+    cudaMemcpy(LandD, gpuA, sizeof(float) * n * (d + d * (d + 1) / 2), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    // cudaMemcpy(X, gpuY, sizeof(float) * n * d, cudaMemcpyDeviceToHost);
+    // cudaDeviceSynchronize();
+
+
+
+    // printf("[");
+    // for(int k = 0; k<d; k++)
+    //     printf("%f,", X[k]);
+    // printf("]\n");
+    // printf("\0");
+
+    printf("{\n");
+    // print A
+    printf("'A':[");
+    for(int i = 0; i<d; i++){
+      printf("[");
+      for(int j = 0; j<d; j++){
+        if(i==j)
+          printf("%f,",A[hgetD(i)]);
+        else
+          printf("%f,",A[d+hgetL(d,i,j)]);
+      }
+      printf("],");
+    }
+    printf("],\n");
+    // printf("\0");
+
+    // print L
+    printf("'L':[");
+    for(int i = 0; i<d; i++){
+      printf("[");
+      for(int j = 0; j<d; j++){
+        if(j>i)
+          printf("%f,",0.0f);
+        else
+          printf("%f,",LandD[d+hgetL(d,i,j)]);
+      }
+      printf("],");
+    }
+    printf("],\n");
+    // printf("\0");
+
+    // print D
+    printf("'D':[");
+    for(int i = 0; i<d; i++)
+      printf("%f,",LandD[hgetD(i)]);
+    printf("],\n");
+    // printf("\0");
+
+    // print ones
+    printf("'ones':[");
+    for(int i = 0; i<d; i++)
+      printf("%f,",A[d+hgetL(d, i, i)]);
+    printf("],\n");
+    // printf("\0");
+
+    printf("}\n");
+
+
+    cudaFree(gpuA);
+    cudaFree(gpuY);
+    free(A);
+    free(LandD);
+    free(Y);
+    // free(Ychap);
+    // free(X);
+
+    return 0;
 }
--- main.cu
+++ main.cu
@@ -6,22 +6,79 @@ Pierre Guetschel
 #include <iostream>
 
 
-// Genrates Gaussian distribution from a uniform one (Box-Muller)
-__device__ void BoxMuller_d(float *g0, float *g1) {
-
-    float loc;
-    if (*g1 < 1.45e-6f) {
-        loc = sqrtf(-2.0f * logf(0.00001f)) * cosf(*g0 * 2.0f * MoPI);
-    } else {
-        if (*g1 > 0.99999f) {
-            loc = 0.0f;
-        } else { loc = sqrtf(-2.0f * logf(*g1)) * cosf(*g0 * 2.0f * MoPI); }
+int hgetL(int d, int i, int j) {
+    // If j > i, then we take the transpose of L
+    if (j > i) {
+        int t = i;
+        i = j;
+        j = t;
     }
-    *g0 = loc;
+
+    int l_position = i * (i - 1) / 2 + j-1;
+
+    return l_position;
 }
 
-// Monte Carlo routine
-__global__ void LDLt_max_k(int AGPU, int YGPU, int d) {
+int hgetD(int i) {
+    int d_position = i;
+    return d_position;
+}
+
+void generate_systems(float *A, float *Y, int N, int d, bool verbose=true) {
+    int matrix_size = d + d * (d + 1) / 2;
+
+    for (int i = 0; i < N; i++) {
+        float *D = &A[i * matrix_size];
+        float *T = &A[i * matrix_size + d];
+
+
+        for (int j = 0; j < (d * (d + 1) / 2); j++)
+            T[j] = ((float) (1+rand()%2));
+
+
+        for (int j = 1; j<=d; j++)
+            T[hgetL(d, j,j)] = 1.0f;
+
+        for (int j = 0; j < d; j++) {
+            D[j] = (float) (1+rand()%5);
+            Y[matrix_size * i + j] = ((float) (1+rand()%2));
+        }
+
+        if (verbose)
+        {
+            printf("[");
+            for(int k = 1; k<=d; k++)
+            {
+                printf("[");
+                for(int j = 1; j<=d; j++)
+                {
+                    if (j <= k)
+                        printf("%f,", T[hgetL(d, j, k)]);
+                    else
+                        printf("%f,", 0.0f);
+                }
+                printf("],");
+            }
+            printf("]\n");
+        }
+
+        printf("[");
+        for(int k = 0; k<d; k++)
+            printf("%f,", D[k]);
+        printf("]\n");
+
+        printf("[");
+        for(int k = 0; k<d; k++)
+            printf("%f,", Y[k]);
+        printf("]\n");
+
+    }
+}
+
+// ************************************************************************ //
+
+
+__global__ void LDLt_max_k(int AGPU, int d) {
     int tidx = threadIdx.x % d;
     int Qt = (threadIdx.x - tidx) / d;
     int gbx = Qt + blockIdx.x * (blockDim.x / d);
@@ -33,19 +90,21 @@ __global__ void LDLt_max_k(int AGPU, int YGPU, int d) {
     for (i = n; i > 0; i--) {
         if (tidx == 0) {
             for (k = n; k > i; k--) {
-                sA[nt + n2 - i * (i + 1) / 2] -= sA[nt + n2 - k * (k + 1) / 2] *
-                                                 sA[nt + n2 - k * (k + 1) / 2 + k - i] *
-                                                 sA[nt + n2 - k * (k + 1) / 2 + k - i];
+                sA[nt + n2 - i * (i + 1) / 2] -=
+                  sA[nt + n2 - k * (k + 1) / 2] *
+                  sA[nt + n2 - k * (k + 1) / 2 + k - i] *
+                  sA[nt + n2 - k * (k + 1) / 2 + k - i];
             }
         }
         __syncthreads();
         if (tidx < i - 1) {
             sA[nt + n2 - i * (i + 1) / 2 + tidx + 1] /= sA[nt + n2 - i * (i + 1) / 2];
             for (k = n; k > i; k--) {
-                sA[nt + n2 - i * (i + 1) / 2 + tidx + 1] -= sA[nt + n2 - k * (k + 1) / 2] *
-                                                            sA[nt + n2 - k * (k + 1) / 2 + k - i] *
-                                                            sA[nt + n2 - k * (k + 1) / 2 + tidx + 1 + k - i] /
-                                                            sA[nt + n2 - i * (i + 1) / 2];
+                sA[nt + n2 - i * (i + 1) / 2 + tidx + 1] -=
+                  sA[nt + n2 - k * (k + 1) / 2] *
+                  sA[nt + n2 - k * (k + 1) / 2 + k - i] *
+                  sA[nt + n2 - k * (k + 1) / 2 + tidx + 1 + k - i] /
+                  sA[nt + n2 - i * (i + 1) / 2];
             }
         }
         __syncthreads();
@@ -53,34 +112,113 @@ __global__ void LDLt_max_k(int AGPU, int YGPU, int d) {
 
 }
 
+// ************************************************************************ //
+
 
 int main() {
     float Tim;                            // GPU timer instructions
     cudaEvent_t start, stop;            // GPU timer instructions
+    int d = 11;
+    int n = 1;
 
-    cudaMalloc(&res1, sizeof(float));
-    cudaMemset(res2, 0.0f, sizeof(float));
+    srand(time(0));
+
+    auto *A     = (float *) malloc(sizeof(float) * n * (d + d * (d + 1) / 2));
+    auto *LandD = (float *) malloc(sizeof(float) * n * (d + d * (d + 1) / 2));
+    auto *Y = (float *) malloc(sizeof(float) * n * d);
+    // auto *Ychap = (float *) malloc(sizeof(float) * n * d);
+    auto *X = (float *) malloc(sizeof(float) * n * d);
+
+    float *gpuA;
+    float *gpuY;
+
+    cudaMalloc(&gpuA, sizeof(float) * n * (d + d * (d + 1) / 2));
+    cudaMalloc(&gpuY, sizeof(float) * n * d);
+
+    generate_systems(A, Y, n, d);
+
+    cudaMemcpy(gpuA, A, sizeof(float) * n * (d + d * (d + 1) / 2), cudaMemcpyHostToDevice);
+    // cudaMemcpy(gpuY, Y, sizeof(float) * n * d, cudaMemcpyHostToDevice);
 
 
-    cudaEventCreate(&start);            // GPU timer instructions
-    cudaEventCreate(&stop);                // GPU timer instructions
-    cudaEventRecord(start, 0);            // GPU timer instructions
 
-    LDLt_max_k << < NB, d * minTB, minTB * ((d * d + d) / 2 + d) * sizeof(float) >> > (AGPU, YGPU, d);
+    cudaEventCreate(&start);                // GPU timer instructions
+    cudaEventCreate(&stop);                 // GPU timer instructions
+    cudaEventRecord(start, 0);              // GPU timer instructions
 
-    cudaEventRecord(stop, 0);            // GPU timer instructions
-    cudaEventSynchronize(stop);            // GPU timer instructions
-    cudaEventElapsedTime(&Tim,            // GPU timer instructions
-                         start, stop);                // GPU timer instructions
-    cudaEventDestroy(start);            // GPU timer instructions
-    cudaEventDestroy(stop);                // GPU timer instructions
+    LDLt_max_k << < NB, d * minTB, minTB * ((d * d + d) / 2 + d) * sizeof(float) >> > (gpuA, d);
+    cudaDeviceSynchronize();
 
-
-    cudaMemcpy(&sum, res1, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(res1);
+    cudaEventRecord(stop, 0);               // GPU timer instructions
+    cudaEventSynchronize(stop);             // GPU timer instructions
+    cudaEventElapsedTime(&Tim, start, stop);// GPU timer instructions
+    cudaEventDestroy(start);                // GPU timer instructions
+    cudaEventDestroy(stop);                 // GPU timer instructions
+    printf("Execution time %f ms\n", Tim);  // GPU timer instructions
 
 
-    printf("Execution time %f ms\n", Tim);
+    cudaMemcpy(LandD, gpuA, sizeof(float) * n * (d + d * (d + 1) / 2), cudaMemcpyDeviceToHost);
+    cudaDeviceSynchronize();
+    // cudaMemcpy(X, gpuY, sizeof(float) * n * d, cudaMemcpyDeviceToHost);
+    // cudaDeviceSynchronize();
+
+
+
+    // printf("[");
+    // for(int k = 0; k<d; k++)
+    //     printf("%f,", X[k]);
+    // printf("]\n");
+    // printf("\0");
+
+    // print A
+    printf("This is A :\n");
+    printf("[");
+    for(int i = 0; i<d; i++){
+      printf("[");
+      for(int k = 0; j<d; j++){
+        if(i==j)
+          printf("%f,",A[hgetD(i)]);
+        else
+          printf("%f,",A[hgetL(d,i,j)]);
+      }
+      printf("],");
+    }
+    printf("]\n");
+    printf("\0");
+
+    // print L
+    printf("This is L :\n");
+    printf("[");
+    for(int i = 0; i<d; i++){
+      printf("[");
+      for(int k = 0; j<d; j++){
+        if(j>i)
+          printf("%f,",0.0f);
+        else
+          printf("%f,",LandD[hgetL(d,i,j)]);
+      }
+      printf("],");
+    }
+    printf("]\n");
+    printf("\0");
+
+    // print D
+    printf("This is D :\n");
+    printf("[");
+    for(int i = 0; i<d; i++)
+      printf("%f,",LandD[hgetD(i)]);
+    printf("]\n");
+    printf("\0");
+
+
+
+    cudaFree(gpuA);
+    cudaFree(gpuY);
+    free(A);
+    free(LandD);
+    free(Y);
+    // free(Ychap);
+    // free(X);
 
     return 0;
 }
